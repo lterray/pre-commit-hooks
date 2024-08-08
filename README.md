caf432c7c9d2f06481fa99580d37d208ea536374
Laszlo Terray <laszlo.terray@gmail.com>
2024-08-08 15:22:50 +0200

    Add skeleton of pre-commit without hook logic

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # pre-commit-hooks
 
 My pre-commit hooks for the [pre-commit python framework](https://pre-commit.com/)
+
+## numeric-ticket-id-to-prepare-commit-msg
+
+This hook tries to parse out numeric ticket id from branch name to prepend to commit message
---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import find_packages
+from setuptools import setup
+
+setup(
+    name='pre-commit-hooks-bandit',
+    description='A pre-commit hook to parse branch name to find ticket id to prepend to commit message',
+    url='https://github.com/lterray/pre-commit-hooks',
+    version='0.0.1',
+
+    author='Laszlo Terray',
+
+    platforms='linux',
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.9',
+    ],
+
+    packages=find_packages('.'),
+    install_requires=[],
+    entry_points={
+        'console_scripts': [
+            'prepend_numeric_ticket_id = pre_commit_hooks.prepend_numeric_ticket_id:main',
+        ],
+    },
+)
